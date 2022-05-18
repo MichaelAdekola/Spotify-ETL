50134e3ba70c32b19f86815525f015d9a8d68305
MichaelAdekola <michaelscott658@gmil.com>
2022-05-19 00:13:31 +1000

    updated code file

--- dags/spotify_dag.py
+++ dags/spotify_dag.py
@@ -1,14 +1,15 @@
-from datetime import timedelta
+from datetime import timedelta, datetime
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 
+#import spotifyEtl
 from spotify_etl import run_spotify_etl
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2020, 11, 8),
+    'start_date': datetime(2022, 5, 18),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -19,6 +20,7 @@ default_args = {
 dag = DAG(
     dag_id='spotify_dag',
     default_args=default_args,
+    #start_date=datetime(2022, 5, 16),
     description='first spotify DAG',
     schedule_interval=timedelta(days=1),
 )
